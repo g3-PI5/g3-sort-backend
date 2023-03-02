d2976384f4f365ca0fedf22b2b6acc66e2b3e40e
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-03-02 16:56:29 -0300

    Update random.js

--- util/random.js
+++ util/random.js
@@ -2,34 +2,29 @@
 //Função para gerar um númeor aleatório e inteiro entre valor min e máx
 
 function random(quantidade) {
+	function intaleatorio(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
 
-    function intaleatorio(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
+	//Array que armazena as sequências
+	let sequencias = [];
 
-    //Array que armazena as sequências
-    let sequencias = [];
+	//Gerar 5 sequências e quantidade de números dentro delas
+	for (let i = 0; i < 5; i++) {
+		//Array que armazena a sequência
+		let sequencia = [];
 
-    //Gerar 5 sequências e quantidade de números dentro delas
-    for (let i = 0; i < 5; i++) {
+		//Gerar os números da sequência
+		for (let j = 0; j < quantidade; j++) {
+			let numero = intaleatorio(20, 2000000);
 
-        //Array que armazena a sequência 
-        let sequencia = [];
+			sequencia.push(numero);
+		}
 
-        //Gerar os números da sequência
-        for (let j = 0; j < quantidade; j++) {
-            let numero = intaleatorio(20, 2000000);
+		sequencias.push(sequencia);
+	}
 
-            sequencia.push(numero);
-        }
-
-        sequencias.push(sequencia);
-    }
-
-    return sequencias;
-
+	return sequencias;
 }
 
-console.log(random(10000))
-
-module.exports = random
+module.exports = random;
